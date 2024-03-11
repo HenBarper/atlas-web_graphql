44f883d39a80365e60fc016ba99d1d66b9c2d0d2
henbarper <benjaminitonio@gmail.com>
2024-03-11 14:56:46 -0500

    1 v1

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,4 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID, GraphQLSchema } = require('graphql');
 
 const TaskType = new GraphQLObjectType({
     name: 'Task',
@@ -9,3 +9,18 @@ const TaskType = new GraphQLObjectType({
         description: { type: GraphQLString }
     }
 });
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        task: {
+            type: TaskType,
+            args: { id: { type: GraphQLID} },
+            resolve(parent,args){
+                //return null;
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery });
