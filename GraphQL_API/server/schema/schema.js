45e0052be64911ed8b2f4f88529571c047f192db
henbarper <benjaminitonio@gmail.com>
2024-03-13 13:53:58 -0500

    8 v1

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -3,37 +3,37 @@ const lodash = require('lodash');
 const Project = require('../models/project');
 const Task = require('../models/task');
 
-const arrayOfTasks = [
-    {
-        id: '1',
-        title: 'Create your first webpage',
-        weight: 1,
-        description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
-        projectId: '1'
-    },
-    {
-        id: '2',
-        title: 'Structure your webpage',
-        weight: 1,
-        description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
-        projectId: '1'
-    }
-]
+// const arrayOfTasks = [
+//     {
+//         id: '1',
+//         title: 'Create your first webpage',
+//         weight: 1,
+//         description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+//         projectId: '1'
+//     },
+//     {
+//         id: '2',
+//         title: 'Structure your webpage',
+//         weight: 1,
+//         description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+//         projectId: '1'
+//     }
+// ]
 
-const arrayOfProjects = [
-    {
-        id: '1',
-        title: 'Advanced HTML',
-        weight: 1,
-        description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'
-    },
-    {
-        id: '2',
-        title: 'Bootstrap',
-        weight: 1,
-        description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'
-    }
-]
+// const arrayOfProjects = [
+//     {
+//         id: '1',
+//         title: 'Advanced HTML',
+//         weight: 1,
+//         description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'
+//     },
+//     {
+//         id: '2',
+//         title: 'Bootstrap',
+//         weight: 1,
+//         description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'
+//     }
+// ]
 
 const TaskType = new GraphQLObjectType({
     name: 'Task',
@@ -45,7 +45,8 @@ const TaskType = new GraphQLObjectType({
         project: {
             type: ProjectType,
             resolve(parent, args){
-                return lodash.find(arrayOfProjects, { 'id':  parent.projectId })
+                // return lodash.find(arrayOfProjects, { 'id':  parent.projectId })
+                return Project.find({ 'id':  parent.projectId })
             }
         }
     })
@@ -61,7 +62,8 @@ const ProjectType = new GraphQLObjectType({
         tasks: {
             type: new GraphQLList(TaskType),
             resolve(parent, args){
-                return lodash.filter(arrayOfTasks, {'projectId': parent.id})
+                // return lodash.filter(arrayOfTasks, {'projectId': parent.id})
+                return Task.find({ 'projectId':  parent.id })
             }
         }
     })
@@ -74,7 +76,8 @@ const RootQuery = new GraphQLObjectType({
             type: TaskType,
             args: { id: { type: GraphQLID} },
             resolve(parent, args){
-                return lodash.find(arrayOfTasks, { 'id': args.id })
+                // return lodash.find(arrayOfTasks, { 'id': args.id })
+                return Task.find({ 'id':  args.id })
             }
         },
         tasks: {
@@ -87,7 +90,8 @@ const RootQuery = new GraphQLObjectType({
             type: ProjectType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args){
-                return lodash.find(arrayOfProjects, { 'id': args.id })
+                // return lodash.find(arrayOfProjects, { 'id': args.id })
+                return Project.find({ 'id':  args.id })
             }
         },
         projects: {
@@ -123,13 +127,15 @@ const Mutation = new GraphQLObjectType({
             args: {
                 title: { type: new GraphQLNonNull(GraphQLString) },
                 weight: { type: new GraphQLNonNull(GraphQLInt) },
-                description: { type: new GraphQLNonNull(GraphQLString) }
+                description: { type: new GraphQLNonNull(GraphQLString) },
+                projectId: { type: new GraphQLNonNull(GraphQLID) }
             },
             resolve(parent, args){
                 const newTask = new Task({
                     title: args.title,
                     weight: args.weight,
-                    description: args.description
+                    description: args.description,
+                    projectId: args.projectId
                 });
                 return newTask.save();
             }
